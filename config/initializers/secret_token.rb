fc9e6e3881f62993dfffc92cec3e040ce81448a0
HasegawaTadamitsu <hasegawa.tadamitsu@gmail.com>
2011-11-23 14:56:26 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gamenseni::Application.config.secret_token = 'fa77097d55b5dc50b5bc67d428bb22c0cbfb24f2cba1f188d2725ba0fc878daed26fbc84ab23335be596b63f698d40d86b7d2d972d196e021b450c25b694b977'
