c5b9e102236cab5a345aae7d199d09bd38492621
HasegawaTadamitsu <hasegawa.tadamitsu@gmail.com>
2011-11-23 16:15:02 +0900

    session ok

--- app/controllers/gamen_controller.rb
+++ app/controllers/gamen_controller.rb
@@ -3,23 +3,37 @@ class GamenController < ApplicationController
   respond_to :html
 
   def new
-    @gamen = Gamen.new
+    @gamen = session[:gamen]
+    if @gamen.nil?
+      @gamen = Gamen.new
+    end
   end
 
   def confirm
-    @gamen = Gamen.new
+    para = params[:gamen]
+    if para.nil?
+      raise "bad request? params is nil."
+    end
+    @gamen = Gamen.new(para)
+    session[:gamen] = @gamen
     if not @gamen.save
       render :action => "new" 
     end
   end
 
   def reedit
-    @gamen = Gamen.new
+    @gamen = session[:gamen]
+    if @gamen.nil?
+      raise "bad request? session is nil."
+    end
     render :action => "new"
   end
 
   def create
-    @gamen = Gamen.new
+    @gamen = session[:gamen]
+    if @gamen.nil?
+      raise "bad request? session is nil."
+    end
     if not @gamen.save
       render :action => "new" 
     end
@@ -27,6 +41,11 @@ class GamenController < ApplicationController
   end
 
   def complete
+    @gamen = session[:gamen]
+    if @gamen.nil?
+      raise "bad request? session is nil."
+    end
+    session[:gamen] = nil
     @data=Hash.new
     @data[:id] = 'data'
     respond_with  @data
