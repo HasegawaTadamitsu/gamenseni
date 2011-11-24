cc8335743826121ab82d083271fffb11d28fb3ac
HasegawaTadamitsu <hasegawa.tadamitsu@gmail.com>
2011-11-25 05:22:37 +0900

    my select

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def hissu
+    "*"
+  end
+
+  def my_select model, attr, hash, default_value
+    tmp_hash=Hash.new
+    hash.each do|key,value|
+      tmp_hash[value]=key
+    end
+    select model,attr,tmp_hash,:selected=>default_value
+  end
+
+  def my_label_from_hash hash, key
+    hash[key.to_i]
+  end
+
 end
--- app/models/gamen.rb
+++ app/models/gamen.rb
@@ -1,6 +1,17 @@
+# -*- coding: utf-8 -*-
 class Gamen < ActiveRecord::Base
   validates :title,
               :presence=>true,
               :numericality => true,
               :length=>{ :minimum =>10 },:allow_blank=>true
+
+  def self.select_data
+    {
+    0=>"現在の時刻",
+    1=>"最後にアクセスしてから"
+    }
+  end
+
+  attr_accessor :select_value
+
 end
